5a79fe56735b775213b897070b5c6890ed8dd202
15951668720 <sh195448@alibaba-inc.com>
2020-03-21 22:10:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,127 @@
+# mybatis-generator-core-fix
+## branches-swagger-tkmapper 整合分支 
+本工程为maven项目，可以直接打包使用
+此分支整合了swagger 和 通用mapper 以及hibernate validators 的校验
+最终的代码样式：
+### mapper接口样式：因为整合了通用mapper，故只需继承Mapper,基本的单表操作完全有通用mapper完成，负责的操作还是需要使用自定义mapper接口，完全是没有影响的
+```
+package com.ecps.ssm.mapper;
+
+import com.ecps.ssm.pojo.TbOrder;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface TbOrderMapper extends Mapper<TbOrder> {
+}
+```
+### mapper.xml样式：因为整合了通用mapper，故只生产基本的resultMap和sqlColumn
+```
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE mapper PUBLIC "-//mybatis.org//DTD Mapper 3.0//EN" "http://mybatis.org/dtd/mybatis-3-mapper.dtd">
+<mapper namespace="com.ecps.ssm.mapper.TbOrderMapper">
+    <resultMap id="BaseResultMap" type="com.ecps.ssm.pojo.TbOrder">
+        <!--@mbggenerated-->
+        <id column="order_id" jdbcType="VARCHAR" property="orderId" />
+        <result column="payment" jdbcType="VARCHAR" property="payment" />
+        <result column="payment_type" jdbcType="INTEGER" property="paymentType" />
+        
+    </resultMap>
+    <sql id="Base_Column_List">
+        <!--@mbggenerated-->
+        order_id, payment, payment_type
+    </sql>
+</mapper>
+```
+### model的样式：
+```
+package com.ecps.ssm.pojo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@ApiModel(value="订单表",description="数据库表：tb_order")@Table(name = "tb_order")//swagger model注解
+public class TbOrder {
+
+    @ApiModelProperty(value="订单id",name="orderId", required=true)//swagger 属性 字段 注解
+    @NotEmpty  //此处是hibernate validator的注解
+    @Id //此处是通用mapper的注解
+    @Column(name = "order_id") //此处是通用mapper的注解 如果数据库标准的下划线，则不用此注解也是可以的
+    private String orderId;
+
+    @ApiModelProperty(value="实付金额。精确到2位小数;单位:元。如:200.07，表示:200元7分",name="payment")
+    @Column(name = "payment")
+    private String payment;
+
+    @ApiModelProperty(value="支付类型，1、在线支付，2、货到付款",name="paymentType")
+    @Column(name = "payment_type")
+    private Integer paymentType;
+
+
+    /**
+     * 获取 订单id 字段:tb_order.order_id
+     *
+     * @return tb_order.order_id, 订单id
+     */
+    public String getOrderId() {
+        return orderId;
+    }
+
+    /**
+     * 设置 订单id 字段:tb_order.order_id
+     *
+     * @param orderId the value for tb_order.order_id, 订单id
+     */
+    public void setOrderId(String orderId) {
+        this.orderId = orderId == null ? null : orderId.trim();
+    }
+
+    /**
+     * 获取 实付金额。精确到2位小数;单位:元。如:200.07，表示:200元7分 字段:tb_order.payment
+     *
+     * @return tb_order.payment, 实付金额。精确到2位小数;单位:元。如:200.07，表示:200元7分
+     */
+    public String getPayment() {
+        return payment;
+    }
+
+    /**
+     * 设置 实付金额。精确到2位小数;单位:元。如:200.07，表示:200元7分 字段:tb_order.payment
+     *
+     * @param payment the value for tb_order.payment, 实付金额。精确到2位小数;单位:元。如:200.07，表示:200元7分
+     */
+    public void setPayment(String payment) {
+        this.payment = payment == null ? null : payment.trim();
+    }
+
+    /**
+     * 获取 支付类型，1、在线支付，2、货到付款 字段:tb_order.payment_type
+     *
+     * @return tb_order.payment_type, 支付类型，1、在线支付，2、货到付款
+     */
+    public Integer getPaymentType() {
+        return paymentType;
+    }
+
+    /**
+     * 设置 支付类型，1、在线支付，2、货到付款 字段:tb_order.payment_type
+     *
+     * @param paymentType the value for tb_order.payment_type, 支付类型，1、在线支付，2、货到付款
+     */
+    public void setPaymentType(Integer paymentType) {
+        this.paymentType = paymentType;
+    }
+
+}
+```
++ 类
+    + JavaMapperGenerator
+    + BaseRecordGenerator
+    + JavaMapperGenerator
+    + ExampleGenerator
+
+
---
+++ src/main/java/org/mybatis/generator/internal/DG2CommentGenerator.java
@@ -0,0 +1,157 @@
+package org.mybatis.generator.internal;
+
+import org.mybatis.generator.api.CommentGenerator;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.TextElement;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+
+import java.util.Iterator;
+
+/**
+ * 自定义注解生成
+ *
+ * @author IT_donggua
+ * @version V1.0
+ * @create 2016-09-02 下午 06:28
+ */
+public class DG2CommentGenerator extends DefaultCommentGenerator {
+
+    //生成model对象的注释信息
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
+
+        // 修改 ApiModel value 值为类名称，为了可以使用 swagger-codegen 来生成客户端代码。
+        // codegen 不支持中文的 ApiModel。
+        StringBuffer sb = new StringBuffer();
+        sb.append("@ApiModel(\n\t\tvalue=\"")
+                .append(innerClass.getType().getShortName())
+                .append("\",\n\t\tdescription=\"数据库表：")
+                .append(introspectedTable.getFullyQualifiedTable())
+                .append(":")
+                .append(introspectedTable.getFullyQualifiedTable().getRemark())
+                .append ("\")");
+//        sb.append("@Data");
+        //添加通用mapper注释 @Table(name = "数据库表名")
+        sb.append("\n@Table(name = \"" + introspectedTable.getFullyQualifiedTable() + "\")");
+        innerClass.addJavaDocLine(sb.toString());
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
+        // 类注释，不管用
+        String shortName = innerClass.getType().getShortName();
+        innerClass.addJavaDocLine("/**");
+        innerClass.addJavaDocLine(" * 类注释");
+        innerClass.addJavaDocLine(" * " + shortName);
+        innerClass.addJavaDocLine(" * 数据库表：" + introspectedTable.getFullyQualifiedTable());
+        // addJavadocTag(innerClass, false);
+        innerClass.addJavaDocLine(" */");
+    }
+
+    //model对象中字段的注释
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+
+        StringBuffer sb = new StringBuffer();
+
+        if (introspectedColumn.getRemarks() != null) {
+            sb.append("@ApiModelProperty(value=\"").append(introspectedColumn.getRemarks()).append("\",name=\"").append(introspectedColumn.getJavaProperty());
+        }
+
+        if (!(introspectedColumn.isNullable())) {
+            sb.append("\", required=true)");
+            field.addJavaDocLine(sb.toString());
+        } else {
+            sb.append("\")");
+            field.addJavaDocLine(sb.toString());
+        }
+
+        if (!(introspectedColumn.isNullable())) {
+            field.addAnnotation("@NonNull");
+        }
+
+        //添加通用mapper字段注解  @Column(name = "数据库字段")
+        //如果是主键，需要添加@Id
+        Iterator<IntrospectedColumn> primaryKeys = introspectedTable.getPrimaryKeyColumns().iterator();
+        if (primaryKeys.hasNext()) {
+            IntrospectedColumn primaryColumn1 = primaryKeys.next();
+            if (introspectedColumn == primaryColumn1) {
+                field.addAnnotation("@Id");
+            }
+        }
+
+        field.addAnnotation("@Column(name = \"`" + introspectedColumn.getActualColumnName() + "`\")");
+
+    }
+
+    @Override
+    public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        StringBuilder sb = new StringBuilder();
+
+        method.addJavaDocLine("/**"); //$NON-NLS-1$
+
+        sb.append(" * 获取 "); //$NON-NLS-1$
+        sb.append(introspectedColumn.getRemarks()).append(" 字段:");
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        sb.append('.');
+        sb.append(introspectedColumn.getActualColumnName());
+        method.addJavaDocLine(sb.toString());
+
+        method.addJavaDocLine(" *"); //$NON-NLS-1$
+
+        sb.setLength(0);
+        sb.append(" * @return "); //$NON-NLS-1$
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        sb.append('.');
+        sb.append(introspectedColumn.getActualColumnName());
+        sb.append(", ");
+        sb.append(introspectedColumn.getRemarks());
+        method.addJavaDocLine(sb.toString());
+
+        method.addJavaDocLine(" */"); //$NON-NLS-1$
+    }
+
+    @Override
+    public void addSetterComment(Method method,
+                                 IntrospectedTable introspectedTable,
+                                 IntrospectedColumn introspectedColumn) {
+
+        StringBuilder sb = new StringBuilder();
+
+        method.addJavaDocLine("/**"); //$NON-NLS-1$
+
+        sb.append(" * 设置 ");  //$NON-NLS-1$
+        sb.append(introspectedColumn.getRemarks()).append(" 字段:");
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        sb.append('.');
+        sb.append(introspectedColumn.getActualColumnName());
+        method.addJavaDocLine(sb.toString());
+
+        method.addJavaDocLine(" *"); //$NON-NLS-1$
+
+        Parameter parm = method.getParameters().get(0);
+        sb.setLength(0);
+        sb.append(" * @param "); //$NON-NLS-1$
+        sb.append(parm.getName());
+        sb.append(" the value for "); //$NON-NLS-1$
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        sb.append('.');
+        sb.append(introspectedColumn.getActualColumnName());
+        sb.append(", ");
+        sb.append(introspectedColumn.getRemarks());
+        method.addJavaDocLine(sb.toString());
+
+        method.addJavaDocLine(" */"); //$NON-NLS-1$
+    }
+
+    @Override
+    public void addComment(XmlElement xmlElement) {
+
+        StringBuilder sb = new StringBuilder();
+
+        xmlElement.addElement(new TextElement("<!--" + "@mbggenerated" + "-->"));
+
+    }
+}
